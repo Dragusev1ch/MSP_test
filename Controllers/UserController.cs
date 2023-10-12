7c8e3c827b29e3165ac6b42dc981bf32b25e464c
Nazar <nazar.drahusevych@gmail.com>
2023-10-12 10:47:36 +0300

    added a table of participants with the possibility of sorting by columns - name, age, regional center.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using MSP_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSP_test.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var users = db.Users.ToList();
+                return View(users);
+            }
+        }
+    }
+}
